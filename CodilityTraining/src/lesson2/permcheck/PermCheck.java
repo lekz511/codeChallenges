025a5b0b82c7ac604e58fdb9518650c903405d55
Alex A <alejandro.andraca@globant.com>
2014-03-25 17:40:08 -0600

    unified solution

--- CodilityTraining/src/lesson2/permcheck/PermCheck.java
+++ CodilityTraining/src/lesson2/permcheck/PermCheck.java
@@ -9,22 +9,11 @@ public class PermCheck {
 //		int[]A={4, 1, 3};//Sample test case 2 sum 8
 //		int[]A={4, 3, 3};//total sum is correct, but it is not a permutation, N = 3
 		System.out.println(solution(A));
-		System.out.println(solution2(A));
 	}
-    public static int solution(int[] A) {
-    	long n=A.length;
-    	long totalSum=((n*n)+n)/2;//Gauss trick :)
-	    for(int i=0;i<n;i++)
-	    	if(A[i]>n)
-	    		return 0;
-	    	else
-	    		totalSum-=A[i];
-	    
-	    return totalSum==0?1:0;
-    }
-    public static int solution2(int[] A){
+
+    public static int solution(int[] A){
 		long n=A.length;
-		long totalSum=((n*n)+n)/2;//Gauss trick :)
+		long totalSum=((n*n)+n)/2;//Gauss trick
 		HashSet<Integer> ints=new HashSet<Integer>();
 		for(int i=0;i<n;i++)
 			if(!ints.add(A[i]))
